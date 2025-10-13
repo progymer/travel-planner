8bbfd7ea7643518d0745c6c479612b8e7c6536b0
daniel <dany20082011@gmail.com>
2025-10-13 22:12:51 +0300

    first commit

---
+++ prisma/migrations/20251010191726_rename_tittle_to_title/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tittle` on the `Trip` table. All the data in the column will be lost.
+  - Added the required column `title` to the `Trip` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Trip" DROP COLUMN "tittle",
+ADD COLUMN     "title" TEXT NOT NULL;
