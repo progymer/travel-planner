3c54148e38ed74dd9f25abcbf283ef493890c176
daniel <dany20082011@gmail.com>
2026-03-19 21:08:52 +0200

    build the sortable itinerary component

---
+++ components/sortable-itinerary.tsx
@@ -0,0 +1,77 @@
+import { Location } from "@/app/generated/prisma";
+import { DndContext, closestCenter, DragEndEvent } from "@dnd-kit/core"
+import { arrayMove, SortableContext, verticalListSortingStrategy, useSortable } from "@dnd-kit/sortable"
+import { useId, useState } from "react";
+import { CSS } from "@dnd-kit/utilities";
+import { reorderItinerary } from "@/lib/actions/reorder-itinerary";
+
+interface SortableItineraryProps {
+    locations: Location[];
+    tripId: string;
+}
+
+
+function SortableItem({ item }: { item: Location }) {
+
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: item.id });
+
+    return (
+        <div
+            ref={setNodeRef}
+            {...attributes}
+            {...listeners}
+            style={{transform: CSS.Transform.toString(transform), transition}}
+            className="p-4 border rounded-md flex justify-between items-center hover:shadow transition-all"
+        >
+            <div>
+                <h4 className="font-medium text-gray-800">{item.locationName}</h4>
+            </div>
+            <div className="text-sm text-gray-600">
+                Day {item.order + 1}
+            </div>
+        </div>
+    )
+}
+
+
+export default function SortableItinerary({ locations, tripId }: SortableItineraryProps) {
+    const id = useId();
+
+    const [localLocation, setLocalLocation] = useState(locations);
+
+    const handleDragEnd = async (event: DragEndEvent) => {
+        const { active, over } = event
+        
+        if (active.id !== over?.id) {
+
+            const oldIndex = localLocation.findIndex((item) => item.id === active.id);
+            const newIndex = localLocation.findIndex((item) => item.id === over!.id);
+
+            const newLocationsOrder = arrayMove(localLocation, oldIndex, newIndex).map((item, index) => (
+                { ...item, order: index }
+            ))
+
+            setLocalLocation(newLocationsOrder);
+
+            await reorderItinerary(
+                tripId,
+                newLocationsOrder.map((item) => item.id)
+            );
+        }
+    };
+
+    return (
+        <DndContext id={id} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+            <SortableContext
+                items={localLocation.map((loc) => loc.id)}
+                strategy={verticalListSortingStrategy}
+            >
+                <div className="space-y-4">
+                    {localLocation.map((item, index) => (
+                        <SortableItem key={index} item={item} />
+                    ))}
+                </div>
+            </SortableContext>
+        </DndContext>
+    );
+}
